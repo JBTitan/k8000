16bae81ce5e568d9e3fef248a6ff067a77993f20
dnaf <rectar2@gmail.com>
2017-07-20 14:36:20 -0400

    Add MPD module Closes #6

---
+++ src/modules/mpd/index.js
@@ -0,0 +1,37 @@
+const Promise = require("bluebird");
+
+const config = require("config");
+const mpd = require("mpd");
+
+module.exports = {
+	load(k8000, debug) {
+		this.client = Promise.promisifyAll(mpd.connect({
+			host: config.get("mpd.host"),
+			port: config.get("mpd.port")
+		}));
+
+		this.client.on("system-player", () => {
+			debug("Received player event; querying status");
+			return this.client.sendCommandAsync("status")
+				.then(mpd.parseKeyValueMessage)
+				.then((msg) => {
+					debug("Player state is \"%s\"", msg.state);
+					if (msg.state === "play") {
+						return this.client.sendCommandAsync("currentsong")
+							.then(mpd.parseKeyValueMessage)
+							.then((song) => {
+								let playing = (song.Artist || "Unknown") + " - " + (song.Title || "Unknown");
+								debug("Setting playing message to %s", playing);
+								return k8000.user.setGame(playing);
+							});
+					} else {
+						debug("Clearing playing message");
+						k8000.user.setGame();
+					}
+				}).catch(k8000.err);
+		});
+	},
+	unload(k8000, debug) {
+		return this.client.sendCommandAsync("close");
+	}
+}
