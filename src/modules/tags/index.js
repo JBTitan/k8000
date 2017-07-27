fd4ca2cfbd0e2f05b652af3e33b9f988fca822ae
dnaf <rectar2@gmail.com>
2017-07-27 06:30:21 -0400

    Add tags module
    Closes #9

---
+++ src/modules/tags/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+	load() {},
+	unload() {},
+
+	commands: [
+		{
+			aliases: ["tag", "tags", "t"],
+			async fn(args, message, k8000, debug) {
+				args = args.trim().split(" ");
+				const tag = args.shift().trim().toLowerCase();
+				args = args.join(" ").trim();
+
+				if (!tag) { // List tags
+					return message.reply("Not implemented");
+				} else if (args.length === 0) { // Return tag
+					const tagData = k8000.data.get("modules.tags", tag);
+
+					return message.reply(tagData);
+				}  // Set tag
+				k8000.data.set("modules.tags", tag, args);
+
+				return message.reply("Set tag");
+			}
+		}
+	]
+};
